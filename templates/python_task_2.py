c7ed37bd23a603751eb01bb269d1a3ada66218e6
sherylfdo <149571270+sherylfdo@users.noreply.github.com>
2023-12-10 20:32:59 +0530

    python_task_2.py

--- templates/python_task_2.py
+++ templates/python_task_2.py
@@ -10,7 +10,7 @@ def calculate_distance_matrix(df)->pd.DataFrame():
     return distance_matrix
 
 
-def unroll_distance_matrix(distance_matrix)->pd.DataFrame():
+def unroll_distance_matrix(distance_matrix):
     unrolled_matrix = distance_matrix.shape[0]
     unrolled_distances = [[distance_matrix.index[i], distance_matrix.columns[j], distance_matrix.iloc[i, j]]
                           for i in range(unrolled_matrix) for j in range(i + 1, unrolled_matrix) if distance_matrix.iloc[i, j] != 0]
@@ -18,7 +18,7 @@ def unroll_distance_matrix(distance_matrix)->pd.DataFrame():
     return unrolled_df
 
 
-def find_ids_within_ten_percentage_threshold(unrolled_df, ref_val)->pd.DataFrame():
+def find_ids_within_ten_percentage_threshold(unrolled_df, ref_val):
     ref_avg = unrolled_df[unrolled_df['id_start'] == ref_val]['distance'].mean()
     thresh_min = ref_avg - (ref_avg*0.1)
     thresh_max = ref_avg + (ref_avg*0.1)
@@ -28,22 +28,25 @@ def find_ids_within_ten_percentage_threshold(unrolled_df, ref_val)->pd.DataFrame
     return ten_percent_threshold
 
 
-def calculate_toll_rate(unrolled_df)->pd.DataFrame():
+def calculate_toll_rate(unrolled_df):
+    rate_coeff = {'moto': 0.8, 'car': 1.2, 
+                'rv': 1.5, 'bus': 2.2, 'truck': 3.6}
+    for vehicle_types, rate in rate_coeff.items():
+        unrolled_df[vehicle_types] = unrolled_df['distance'] * rate
+    return unrolled_df
 
 
-    return df
+def calculate_time_based_toll_rates(find_ids_within_ten_percentage_threshold):
+    intervals = [
+        (datetime.strptime('00:00:00', '%H:%M:%S').time(), datetime.strptime('10:00:00', '%H:%M:%S').time(), 0.8),
+        (datetime.strptime('10:00:00', '%H:%M:%S').time(), datetime.strptime('18:00:00', '%H:%M:%S').time(), 1.2),
+        (datetime.strptime('18:00:00', '%H:%M:%S').time(), datetime.strptime('23:59:59', '%H:%M:%S').time(), 0.8),
+        (datetime.strptime('00:00:00', '%H:%M:%S').time(), datetime.strptime('23:59:59', '%H:%M:%S').time(), 0.7)]
+    rows = []
+    for id_start, id_end in df[['id_start', 'id_end']].drop_duplicates().itertuples(index=False):
+        for day in range(7):
+            for start_time, end_time, rate in intervals:
+                rows.append([id_start, id_end, datetime(2023, 1, 1) + timedelta(days=day), start_time, datetime(2023, 1, 1) + timedelta(days=day), end_time, rate])
 
-
-def calculate_time_based_toll_rates(df)->pd.DataFrame():
-    """
-    Calculate time-based toll rates for different time intervals within a day.
-
-    Args:
-        df (pandas.DataFrame)
-
-    Returns:
-        pandas.DataFrame
-    """
-    # Write your logic here
-
-    return df
+    time_toll=pd.DataFrame(rows, columns=['id_start', 'id_end', 'start_day', 'start_time', 'end_day', 'end_time', 'rate_factor'])
+    return time_toll
