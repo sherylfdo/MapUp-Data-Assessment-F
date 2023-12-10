6c2af7fefab54aa26c003c4491d1c3b729d27c95
sherylfdo <149571270+sherylfdo@users.noreply.github.com>
2023-12-10 18:09:30 +0530

    python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -2,81 +2,46 @@ import pandas as pd
 
 
 def generate_car_matrix(df)->pd.DataFrame:
-    """
-    Creates a DataFrame  for id combinations.
-
-    Args:
-        df (pandas.DataFrame)
-
-    Returns:
-        pandas.DataFrame: Matrix generated with 'car' values, 
-                          where 'id_1' and 'id_2' are used as indices and columns respectively.
-    """
-    # Write your logic here
-    df=pd.read_csv("dataset-1.csv")
-    df=df.pivot(column='id_1', index='id_2', values='car').fillna(0)
-    for i in df.index.intersection(df.column):
-        df.loc[i,i]=0    
-    return df
+    car_matrix=df.pivot(columns='id_1', index='id_2', values='car').fillna(0)
+    for i in car_matrix.index.intersection(car_matrix.columns):
+        car_matrix.loc[i, i] = 0    
+    return car_matrix
 
 
 def get_type_count(df)->dict:
-    """
-    Categorizes 'car' values into types and returns a dictionary of counts.
-
-    Args:
-        df (pandas.DataFrame)
-
-    Returns:
-        dict: A dictionary with car types as keys and their counts as values.
-    """
-    # Write your logic here
-
-    return dict()
+    dict_car_types={'low':0,'medium':0,'high':0}
+    for i in df['car']:
+        if i<=15:
+            dict_car_types['low']+=1
+        elif 15<i<=25:
+            dict_car_types['medium']+=1
+        else:
+            dict_car_types['high]+=1
+    return dict_car_types
 
 
 def get_bus_indexes(df)->list:
-    """
-    Returns the indexes where the 'bus' values are greater than twice the mean.
-
-    Args:
-        df (pandas.DataFrame)
-
-    Returns:
-        list: List of indexes where 'bus' values exceed twice the mean.
-    """
-    # Write your logic here
-
-    return list()
+    mean_val_bus=df['bus'].mean()
+    indices=df[df['bus']>2*mean_val_bus].index.tolist()
+    sorted_list=sorted(indices)
+    return sorted_list
 
 
 def filter_routes(df)->list:
-    """
-    Filters and returns routes with average 'truck' values greater than 7.
+    truck_routes = df['route'].unique()
+    f = []
 
-    Args:
-        df (pandas.DataFrame)
+    for route in truck_routes:
+        mean_val_truck = df[df['route'] == route]['truck'].mean()
+        if mean_val_truck > 7:
+            f.append(route)
 
-    Returns:
-        list: List of route names with average 'truck' values greater than 7.
-    """
-    # Write your logic here
-
-    return list()
+    filter_route_truck=sorted(f)
+    return filter_route_truck
 
 
-def multiply_matrix(matrix)->pd.DataFrame:
-    """
-    Multiplies matrix values with custom conditions.
-
-    Args:
-        matrix (pandas.DataFrame)
-
-    Returns:
-        pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
-    """
-    # Write your logic here
-
+def multiply_matrix(car_matrix)->pd.DataFrame:
+    
     return matrix
 
 
@@ -93,3 +58,6 @@ def time_check(df)->pd.Series:
     # Write your logic here
 
     return pd.Series()
+
+
+df=pd.read_csv("dataset-1.csv")
