e99b8e9e41ff88917da484b53c3c5f3c9a1112df
sherylfdo <149571270+sherylfdo@users.noreply.github.com>
2023-12-10 19:10:39 +0530

    python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 def generate_car_matrix(df)->pd.DataFrame:
     car_matrix=df.pivot(columns='id_1', index='id_2', values='car').fillna(0)
     for i in car_matrix.index.intersection(car_matrix.columns):
@@ -30,34 +29,36 @@ def get_bus_indexes(df)->list:
 def filter_routes(df)->list:
     truck_routes = df['route'].unique()
     f = []
-
     for route in truck_routes:
         mean_val_truck = df[df['route'] == route]['truck'].mean()
         if mean_val_truck > 7:
             f.append(route)
-
     filter_route_truck=sorted(f)
     return filter_route_truck
 
 
 def multiply_matrix(car_matrix)->pd.DataFrame:
+    multiply_matrix = car_matrix.copy() 
+    for row, rows in multiply_matrix.iterrows():
+        for col in rows.index:
+            value = multiply_matrix.at[row, col]
+            if value > 20:
+                multiply_matrix.at[row, col] *= 0.75
+            else:
+                multiply_matrix.at[row, col] *= 1.25
+            multiply_matrix.at[row, col] = round(multiply_matrix.at[row, col], 1)
+    return multiply_matrix
     
-    return matrix
-
 
 def time_check(df)->pd.Series:
-    """
-    Use shared dataset-2 to verify the completeness of the data by checking whether the timestamps for each unique (`id`, `id_2`) pair cover a full 24-hour and 7 days period
-
-    Args:
-        df (pandas.DataFrame)
-
-    Returns:
-        pd.Series: return a boolean series
-    """
-    # Write your logic here
-
-    return pd.Series()
-
-
-df=pd.read_csv("dataset-1.csv")
+    result1 = []
+    df['startTime'] = pd.to_datetime(df['startDay'] + ' ' + df['startTime'])
+    df['endTime'] = pd.to_datetime(df['endDay'] + ' ' + df['endTime'])
+    grouped = df.groupby(['id', 'id_2'])
+    for (_, _), group_df in grouped:
+        dur_check = (group_df['endTime'].min() - group_df['startTime'].max()) >= pd.Timedelta(days=1)
+        days_check = group_df['startTime'].dt.dayofweek.nunique() == 7
+        result1.append(dur_check and days_check)
+    unique_pairs = df[['id', 'id_2']].drop_duplicates()
+    result = pd.Series(result1, index=unique_pairs.index)
+    return result
